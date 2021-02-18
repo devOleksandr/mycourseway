b9c63d20eb2a5e549f7b53520a74a16f82a2a380
devOleksandr <55914390+devOleksandr@users.noreply.github.com>
2021-02-18 20:51:49 +0200

    add responsive

--- js/main.js
+++ js/main.js
@@ -8,6 +8,15 @@ $( document ).ready(function() {
 
 });
 
+//mobile menu
+$('.mobile-nav__btn').click(function () {
+    $('.mobile-menu').toggleClass('active');
+});
+$('.mobile-menu__link').click(function () {
+    $('.mobile-menu').removeClass('active');
+});
+
+
 <!-- Initialize Swiper -->
 let swiper = new Swiper('.swiper-container', {
     slidesPerView: 4,
@@ -16,4 +25,29 @@ let swiper = new Swiper('.swiper-container', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
+    breakpoints: {
+        // when window width is >= 320px
+        320:{
+            slidesPerView: 1,
+
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1200: {
+            slidesPerView: 3,
+        },
+        1440: {
+            slidesPerView: 4,
+        }
+    }
 });
